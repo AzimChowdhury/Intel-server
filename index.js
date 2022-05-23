9e3356c5b18851587525276744175c2e612f06ed
Azim Chowdhury <azimchy994@gmail.com>
2022-05-23 13:45:52 +0600

    products loaded

--- index.js
+++ index.js
@@ -7,10 +7,39 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Intel-admin:O2kXYBdQVLuvq0Cz@cluster0.6t7o3.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function run() {
+  try {
+    client.connect()
+    const productCollection = client.db("Intel").collection("productCollection");
+    
+    app.get('/products', async(req,res)=>{
+      const result = await productCollection.find().toArray();
+      res.send(result)
+    })
+
+
+  }
+  finally {
+    // client.close()
+  }
+}
+
+
+run()
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Server running !');
+  res.send('Server running !');
 })
 
 app.listen(port, () => {
-    console.log(`server running on the port- ${port}`)
+  console.log(`server running on the port- ${port}`)
 })
