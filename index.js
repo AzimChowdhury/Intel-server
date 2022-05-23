89362f451777729370e44175821e35362855fdae
Azim Chowdhury <azimchy994@gmail.com>
2022-05-23 18:32:01 +0600

    env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Intel-admin:O2kXYBdQVLuvq0Cz@cluster0.6t7o3.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6t7o3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function run() {
