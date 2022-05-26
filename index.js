4dce6f8c8d7d5a9d80463bf016173934f76b69e5
Azim Chowdhury <azimchy994@gmail.com>
2022-05-26 15:00:15 +0600

    product delete api

--- index.js
+++ index.js
@@ -146,6 +146,15 @@ function run() {
       res.send(result)
     })
 
+    //delete a product
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: ObjectId(id) }
+      const result = await productCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
   }
   finally {
