0f0c3dd13999f25b945ef5179a0ce8e1d40bfd93
Azim Chowdhury <azimchy994@gmail.com>
2023-02-06 19:35:53 +0600

    cors issue

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
+app.use(cors()) 
 app.use(express.json())
 
 //error solved
