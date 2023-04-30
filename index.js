e0bb31651715d73700020fb9b8f92b99ca31f5c9
Azim Chowdhury <azimchy994@gmail.com>
2023-04-30 18:17:09 +0800

    cors

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors()) 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://intel-62fc4.firebaseapp.com/");
-  next();
-});
+app.use(cors())
 app.use(express.json())
 
 //error solved
